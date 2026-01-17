1fbb2607bd9d7d6546a8073c1584b11d1e052845
BrunoVieira003 <brunosilverio01@gmail.com>
2026-01-17 19:49:01 -0300

    feat: add artists table and relations
    - Add artists table
    - Create artist on scanning file, if already exists (based on name), use
      the stored artist and create a relation with the song

--- src/lib/processes/scan-file.ts
+++ src/lib/processes/scan-file.ts
@@ -2,8 +2,9 @@ import { parseFile } from "music-metadata"
 import path from "node:path"
 import { Job } from "bee-queue"
 import { db } from "../../database"
-import { songs } from "../../database/schema"
+import { artists, songs, songsToArtists } from "../../database/schema"
 import { filesDir } from "../../constants"
+import { eq } from "drizzle-orm"
 
 export interface ScanFileData {
     filename: string
@@ -19,9 +20,20 @@ export async function scanLocalFile(job: Job<ScanFileData>) {
 
     const title = metadata.common.title || filename
 
-    await db.insert(songs).values({
+    const insertedSong = (await db.insert(songs).values({
         title,
         filename,
         year: metadata.common.year
-    })
+    }).returning())[0]
+
+    const artistTag = metadata.common.artist
+    if(artistTag){
+        const storedArtist = await db.query.artists.findFirst({where: eq(artists.name, artistTag)})
+        if(storedArtist){
+            await db.insert(songsToArtists).values({songId: insertedSong.id, artistId: storedArtist.id})
+        }else{
+            const newArtist = (await db.insert(artists).values({name: artistTag}).returning())[0]
+            await db.insert(songsToArtists).values({songId: insertedSong.id, artistId: newArtist.id})
+        }
+    }
 }
