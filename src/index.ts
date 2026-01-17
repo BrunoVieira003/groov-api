9fb02f16ef06209f0a9797f5f54dccfa52d5e15e
BrunoVieira003 <brunosilverio01@gmail.com>
2026-01-17 02:58:29 -0300

    fix: add cors config

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { Elysia } from "elysia";
 import { songRouter } from "./song/controller";
 import { migrate } from 'drizzle-orm/bun-sql/migrator'
 import { db } from "./database";
+import cors from "@elysiajs/cors";
 
 await migrate(db, { migrationsFolder: '/drizzle' })
 
 const app = new Elysia()
   .use(songRouter)
+  .use(cors())
   .listen(3000);
 
 console.log(
