81abc38cc54931bec543b964e99e0ff2e1855c63
BrunoVieira003 <brunosilverio01@gmail.com>
2026-01-17 19:49:01 -0300

    feat: add artists table and relations
    - Add artists table
    - Create artist on scanning file, if already exists (based on name), use
      the stored artist and create a relation with the song

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { pgTable, smallint, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const songs = pgTable('songs', {
@@ -8,3 +9,32 @@ export const songs = pgTable('songs', {
     updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
     createdAt: timestamp('created_at').notNull().defaultNow(),
 });
+
+export const songsRelations = relations(songs, ({many}) => ({
+    authors: many(songsToArtists)
+}))
+
+export const artists = pgTable('artists', {
+    id: uuid().primaryKey().defaultRandom(),
+    name: varchar('name').notNull()
+})
+
+export const artistsRelations = relations(artists, ({many}) => ({
+    songs: many(songsToArtists)
+}))
+
+export const songsToArtists = pgTable('songs_to_artists', {
+    songId: uuid().notNull().references(() => songs.id),
+    artistId: uuid().notNull().references(() => artists.id)
+})
+
+export const songsToArtistsRelations = relations(songsToArtists, ({one}) => ({
+    song: one(songs, {
+        fields: [songsToArtists.songId],
+        references: [songs.id]
+    }),
+    artist: one(artists, {
+        fields: [songsToArtists.artistId],
+        references: [artists.id]
+    })
+}))
