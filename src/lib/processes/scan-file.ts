484ba6fea08c6b4996b7a66c73d438f8185c2a47
BrunoVieira003 <brunosilverio01@gmail.com>
2026-01-18 01:26:18 -0300

    fix: avoid creating duplicates on database when scanning
    - If a song already exists, the stored song is used
    - If an artist already exists, the stored artist is used
    - If relation between song and artist already exists, no relation is
      created

--- src/lib/processes/scan-file.ts
+++ src/lib/processes/scan-file.ts
@@ -4,7 +4,7 @@ import { Job } from "bee-queue"
 import { db } from "../../database"
 import { artists, songs, songsToArtists } from "../../database/schema"
 import { filesDir } from "../../constants"
-import { eq } from "drizzle-orm"
+import { and, eq } from "drizzle-orm"
 
 export interface ScanFileData {
     filename: string
@@ -25,23 +25,35 @@ export async function scanLocalFile(job: Job<ScanFileData>) {
 
     const title = metadata.common.title || filename
 
-    const insertedSong = (await db.insert(songs).values({
-        title,
-        filename,
-        year: metadata.common.year
-    }).returning())[0]
+    let song = await db.query.songs.findFirst({where: eq(songs.filename, filename), with: { authors: true } })
+    if(!song){
+        const newSong = (await db.insert(songs).values({
+            title,
+            filename,
+            year: metadata.common.year
+        }).returning())[0]
+
+        song = {...newSong, authors: [] }
+    }
 
     const artistTag = metadata.common.artist
     if(artistTag){
         const artistNames = parseArtistTag(artistTag)
         for(let art of artistNames){
-            const storedArtist = await db.query.artists.findFirst({where: eq(artists.name, art)})
-            if(storedArtist){
-                await db.insert(songsToArtists).values({songId: insertedSong.id, artistId: storedArtist.id})
-            }else{
+            let artist = await db.query.artists.findFirst({where: eq(artists.name, art)})
+            if(!artist){
                 const newArtist = (await db.insert(artists).values({name: art}).returning())[0]
-                await db.insert(songsToArtists).values({songId: insertedSong.id, artistId: newArtist.id})
+                artist = newArtist
             }
+
+            const songArtistRelation = await db.query.songsToArtists.findFirst({where: and(
+                eq(songsToArtists.songId, song.id),
+                eq(songsToArtists.artistId, artist.id)
+            )})
+
+            if(!songArtistRelation){
+                await db.insert(songsToArtists).values({songId: song.id, artistId: artist.id})
+            }  
         }
     }
 }
